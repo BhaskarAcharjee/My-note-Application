3384bac91aaa8161110c300b58692441817420db
BhaskarAcharjee <bhaskaracharjee2002@gmail.com>
2022-08-15 19:19:18 +0530

    Update Note features & Scroll View integrated

--- app/src/main/java/com/bhaskar/mynote/DataInsertActivity.java
+++ app/src/main/java/com/bhaskar/mynote/DataInsertActivity.java
@@ -9,7 +9,7 @@ import android.view.View;
 import com.bhaskar.mynote.databinding.ActivityDataInsertBinding;
 
 public class DataInsertActivity extends AppCompatActivity {
-ActivityDataInsertBinding binding;
+    ActivityDataInsertBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,19 +17,40 @@ ActivityDataInsertBinding binding;
         binding = ActivityDataInsertBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        String type = getIntent().getStringExtra("type");
 
-        binding.add.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent();
-                intent.putExtra("title",binding.title.getText().toString());
-                intent.putExtra("disp",binding.disp.getText().toString());
-                setResult(RESULT_OK,intent);
-                finish();
-            }
-        });
+        if (type.equals("update")) {
+            setTitle("update");
+            binding.title.setText(getIntent().getStringExtra("title"));
+            binding.disp.setText(getIntent().getStringExtra("disp"));
+            int id=getIntent().getIntExtra("id",0);
+            binding.add.setText("Update Note");
+            binding.add.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent = new Intent();
+                    intent.putExtra("title", binding.title.getText().toString());
+                    intent.putExtra("disp", binding.disp.getText().toString());
+                    intent.putExtra("id",id);
+                    setResult(RESULT_OK, intent);
+                    finish();
+                }
+            });
+        } else {
+            setTitle("Add Mode");
+            binding.add.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent = new Intent();
+                    intent.putExtra("title", binding.title.getText().toString());
+                    intent.putExtra("disp", binding.disp.getText().toString());
+                    setResult(RESULT_OK, intent);
+                    finish();
+                }
+            });
+        }
     }
-
+//  Back from Add Note Page to Main Page
     @Override
     public void onBackPressed() {
         super.onBackPressed();
--- app/src/main/java/com/bhaskar/mynote/MainActivity.java
+++ app/src/main/java/com/bhaskar/mynote/MainActivity.java
@@ -3,6 +3,7 @@ package com.bhaskar.mynote;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.GridLayout;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -10,6 +11,7 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -19,28 +21,36 @@ import com.bhaskar.mynote.databinding.ActivityMainBinding;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-ActivityMainBinding binding;
-private NoteViewModel noteViewModel;
+    ActivityMainBinding binding;
+    private NoteViewModel noteViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding=ActivityMainBinding.inflate(getLayoutInflater());
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        noteViewModel=new ViewModelProvider(this, (ViewModelProvider.Factory) ViewModelProvider
+        noteViewModel = new ViewModelProvider(this, (ViewModelProvider.Factory) ViewModelProvider
                 .AndroidViewModelFactory.getInstance(this.getApplication())).get(NoteViewModel.class);
         binding.floatingActionButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent=new Intent(MainActivity.this,DataInsertActivity.class);
-                startActivityForResult(intent,1);
+                Intent intent = new Intent(MainActivity.this, DataInsertActivity.class);
+                intent.putExtra("type", "addMode");
+                startActivityForResult(intent, 1);
             }
         });
 
+//      Scroll Recyclerview in Main_activity (Vertical)
+        LinearLayoutManager manager = new LinearLayoutManager(this);
+        manager.setOrientation(LinearLayoutManager.VERTICAL);
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.Rv);
+        recyclerView.setLayoutManager(manager);
+
+
         binding.Rv.setLayoutManager(new LinearLayoutManager(this));
         binding.Rv.setHasFixedSize(true);
-        RvAdapter adapter= new RvAdapter();
+        RvAdapter adapter = new RvAdapter();
         binding.Rv.setAdapter(adapter);
 
         noteViewModel.getAllNotes().observe(this, new Observer<List<Note>>() {
@@ -50,7 +60,7 @@ private NoteViewModel noteViewModel;
             }
         });
 
-        new ItemTouchHelper(new ItemTouchHelper.SimpleCallback(0,ItemTouchHelper.LEFT|ItemTouchHelper.RIGHT) {
+        new ItemTouchHelper(new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
             @Override
             public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
                 return false;
@@ -60,26 +70,39 @@ private NoteViewModel noteViewModel;
             public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
                 if (direction == ItemTouchHelper.RIGHT) {
                     noteViewModel.delete(adapter.getNote(viewHolder.getAdapterPosition()));
+
                     Toast.makeText(MainActivity.this, "Deleting Notes", Toast.LENGTH_SHORT).show();
                 } else {
-//                    both condition delete note (Updating not enabled still)
-                    noteViewModel.delete(adapter.getNote(viewHolder.getAdapterPosition()));
+                    Intent intent = new Intent(MainActivity.this, DataInsertActivity.class);
+                    intent.putExtra("type", "update");
+                    intent.putExtra("title", adapter.getNote(viewHolder.getAdapterPosition()).getTitle());
+                    intent.putExtra("disp", adapter.getNote(viewHolder.getAdapterPosition()).getDisp());
+                    intent.putExtra("id", adapter.getNote(viewHolder.getAdapterPosition()).getId());
+                    startActivityForResult(intent, 2);
+
                     Toast.makeText(MainActivity.this, "Updating Notes", Toast.LENGTH_SHORT).show();
                 }
             }
-            }).attachToRecyclerView(binding.Rv);
-        }
+        }).attachToRecyclerView(binding.Rv);
+    }
+
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode==1){
+        if (requestCode == 1) {
             String title = data.getStringExtra("title");
             String disp = data.getStringExtra("disp");
-            Note note = new Note(title,disp);
+            Note note = new Note(title, disp);
             noteViewModel.insert(note);
-            Toast.makeText(this,"Note Added Sucessesfully",Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "Note Added Sucessesfully", Toast.LENGTH_SHORT).show();
+        } else if (requestCode == 2) {
+            String title = data.getStringExtra("title");
+            String disp = data.getStringExtra("disp");
+            Note note = new Note(title, disp);
+            note.setId(data.getIntExtra("id", 0));
+            noteViewModel.update(note);
+            Toast.makeText(this, "Note Updated Sucessesfully", Toast.LENGTH_SHORT).show();
         }
-
     }
 }
